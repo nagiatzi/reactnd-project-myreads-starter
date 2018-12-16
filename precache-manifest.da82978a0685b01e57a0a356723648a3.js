d85bd4dd606b767c1f4d8cc28e42cb545fa4c6ba
nagiatzi <nagiatzi@gmail.com>
2018-12-16 16:45:38 +0200

    Updates

---
+++ precache-manifest.da82978a0685b01e57a0a356723648a3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "b3c86e260fc66bbac5b2",
+    "url": "/MyReads/static/css/main.7560ca1e.chunk.css"
+  },
+  {
+    "revision": "b3c86e260fc66bbac5b2",
+    "url": "/MyReads/static/js/main.b3c86e26.chunk.js"
+  },
+  {
+    "revision": "48e1b86bcfbc48ab13b9",
+    "url": "/MyReads/static/js/1.48e1b86b.chunk.js"
+  },
+  {
+    "revision": "970c0060ff174c587b9c",
+    "url": "/MyReads/static/js/runtime~main.970c0060.js"
+  },
+  {
+    "revision": "bb9363b86b02777d23523851826414e0",
+    "url": "/MyReads/static/media/add.bb9363b8.svg"
+  },
+  {
+    "revision": "3687aa988bec28f17ae39153db1646c3",
+    "url": "/MyReads/static/media/arrow-back.3687aa98.svg"
+  },
+  {
+    "revision": "78c6f53b2342643639fe1cbaf171bf47",
+    "url": "/MyReads/static/media/arrow-drop-down.78c6f53b.svg"
+  },
+  {
+    "revision": "dec340f4005891eace18b44b63e56c31",
+    "url": "/MyReads/index.html"
+  }
+];
